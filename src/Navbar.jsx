1459e819828f020d8605b06041b511e4a77287d7
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-01-30 11:50:10 -0500

    updated navbar ui

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,26 +15,72 @@ function Navbar() {
             <div className="bar"></div>
             <div className="bar"></div>
           </div>
-          <div>TEAM TACTICS</div>
+          <h3 id="team-tactics-logo">TEAM TACTICS</h3>
         </div>
 
         <div className="nav-elements-3-4">
-          <div className="profile-pic-small"></div>
+          <div className="profile-pic-small">
+            <p>UP</p>
+          </div>
           <div>User Profile</div>
+
+          {/* Bootstrap icons */}
+          <div className="nav-message-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="currentColor"
+              class="bi bi-envelope-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+            </svg>
+
+            <div className="nav-search-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
 
+      {/* *************
+       Have not decided yet if the side navigation bar will always stay open. It's always open for now.
+       The logic to toggle the side nav is commented out below.
+       ************* */}
+
       {/* This is the side navbar. It uses state "toggleMenu" to open and close the side navbar. */}
-      {toggleMenu && (
-        <div className="side-nav">
-          <div>Dashboard</div>
-          <div>Profile</div>
-          <div>Messages</div>
-          <div>Schedule</div>
-          <div>Manage Teams</div>
-          <div>Logout</div>
+      {/* {toggleMenu && ( */}
+      <div className="side-nav">
+        <div className="profile-pic-large-container">
+          <div className="profile-pic-large">
+            <div>
+              <p>UP</p>
+            </div>
+          </div>
         </div>
-      )}
+        <div id="side-nav-username">
+          <h3>User Profile</h3>
+        </div>
+        <ul>
+          <li>Dashboard</li>
+          <li>Profile</li>
+          <li>Messages</li>
+          <li>Schedule</li>
+          <li>Manage Teams</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+      {/* )} */}
     </>
   );
 }
