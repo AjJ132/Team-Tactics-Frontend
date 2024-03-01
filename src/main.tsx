7c6d931a4fbedffb5c80182677df9201431614a0
AJ Johnson <aj132@icloud.com>
2024-02-29 20:07:04 -0500

    Reworked after merge failure

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import './index.css'
 import Dashboard from './pages/Dashboard/Dashboard'
-import TeamCreation from './pages/TeamCreation/TeamCreation'
 import SignIn from './pages/SigninSignup/Signin'
 import AuthProvider from './providers/AuthProvider'
 import ProtectedRoute from './components/Protected Route/ProtectedRoute'
@@ -26,55 +25,6 @@ import FilesPage from './pages/files/FilesPage'
 import Signup from './pages/SigninSignup/Signup'
 import { ModalVisibilityProvider } from './providers/ModalVisibilityManager'
 
-
-// ------------OLD CODE. Keeping for awhile just in case :) AJ. (Removing this so the navbar doesnt re render on every page change)
-/*
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: 
-      <ProtectedRoute> 
-        <Layout>
-          <Dashboard />
-        </Layout>
-  
-
-      // <ProtectedRoute> {/* 👈 This is the ProtectedRoute component. It ensures that the user is signed in/authenticated before going to this page. */}
-      //   <Layout>
-      //     <Dashboard />
-      //   </Layout>
-      // </ProtectedRoute>,
-  },
-  {
-    path: '/teamcreation',
-    element: 
-    <Layout>
-    <TeamCreation />
-    </Layout>
-  },
-  {
-    path: '/signin',
-    element: <SignIn />,
-  },
-  {
-    path: '/Calendar',
-    element: 
-      <ProtectedRoute>
-        <Layout>
-          <CalendarPage />
-        </Layout>
-      </ProtectedRoute>,
-  },
-  {
-    path: '/messages',
-    element: 
-      <ProtectedRoute>
-        <Layout>
-          <MessagesPage />
-        </Layout>
-      </ProtectedRoute>,
-  },
-]) */
 
 // Adjust your router configuration to include a single layout that wraps around your routes
 const rootElement = document.getElementById('root');
